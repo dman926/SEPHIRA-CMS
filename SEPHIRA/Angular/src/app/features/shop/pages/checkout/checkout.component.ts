9b824f6f45df8106c3fd072b193034ba61e3cc83
DJ Stelmach <dj@dstelmach.com>
2021-12-25 03:07:16 -0500

    rework checkout CSS a little for SSR

--- SEPHIRA/Angular/src/app/features/shop/pages/checkout/checkout.component.ts
+++ SEPHIRA/Angular/src/app/features/shop/pages/checkout/checkout.component.ts
@@ -12,20 +12,22 @@ import { CartService } from '../../services/cart/cart.service';
 })
 export class CheckoutComponent {
 
+	showCheckoutButtons: boolean;
 	readonly enableStripe = environment.enableStripe;
 	readonly enablePayPal = environment.enablePayPal;
 	readonly enableCoinbaseCommerce = environment.enableCoinbaseCommerce;
 	readonly enableNowPayments = environment.enableNowPayments;
 
 	constructor(public cart: CartService, private platform: PlatformService, private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+		// Work around SSR not loading icons correctly
+		// Besides, SSRed page should not be able to checkout. Only checkout from browser
 		if (this.platform.isServer) {
-			this.iconRegistry.addSvgIconLiteral('stripe', this.sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
-			this.iconRegistry.addSvgIconLiteral('paypal', this.sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
-			this.iconRegistry.addSvgIconLiteral('coinbase', this.sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
+			this.showCheckoutButtons = false;
 		} else {
 			this.iconRegistry.addSvgIcon('stripe', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/img/Payment Gateways/Stripe/Stripe wordmark - blurple.svg'));
 			this.iconRegistry.addSvgIcon('paypal', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/img/Payment Gateways/PayPal/PayPal.svg'));
 			this.iconRegistry.addSvgIcon('coinbase', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/img/Payment Gateways/Coinbase/Coinbase_Wordmark.svg'));
+			this.showCheckoutButtons = true;
 		}
 	}
 
